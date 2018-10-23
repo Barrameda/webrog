2d9203c396423dde4e22ab7f587c13fbeaa59588
Barrameda <44312080+Barrameda@users.noreply.github.com>
2018-10-23 11:00:17 +0800

    Add files via upload

---
+++ BadeWebprog/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header.php</title>
+</head>
+<style type="text/css">
+	header
+	
+ul {
+    list-style-type: none;
+    margin: 0 auto;
+    padding: 0;
+    overflow: hidden;
+    background-color: black;
+    float: left;
+    text-align: center;
+    border-radius: 10px;
+
+
+}
+
+.sideheader {
+    margin: 0 auto;
+    
+    
+    
+    
+}
+
+li a {
+    display: block;
+    color: green;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+
+
+}
+
+
+li a:hover {
+    background-color: #111;
+   
+}
+
+
+
+</style>
+<body>
+    <style type="text/css">
+    
+    .body1
+    {
+     
+    }
+</style>
+<header>
+	<ul>
+	<li class = "sideheader"><a href="index.php">Home</a></li>
+	<li class = "sideheader"><a href="link.php">Social Media Accounts/Links</a></li>
+	<li class = "sideheader"><a href="hob.php">Hobbies</a></li>
+	<li class = "sideheader"><a href="etc.php">Achievements</a></li>
+	<li class = "sideheader"><a href="About.php">About Me</a></li>
+</ul>
+</header>
+
+</body>
+</html>
---
+++ BadeWebprog/link.php
@@ -0,0 +1,23 @@
+<?php include("header.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Resume</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<br>
+<br>
+<br>
+<body>
+	<div class = "container">
+		<p>Facebook.com/Bade</p><br>
+		<p>Facebook.com/Bade</p><br>
+		<p>Facebook.com/Bade</p><br>
+		<p>Facebook.com/Bade</p><br>
+		<p>Facebook.com/Bade</p><br>	
+
+
+	</div>
+
+</body>
+</html>
